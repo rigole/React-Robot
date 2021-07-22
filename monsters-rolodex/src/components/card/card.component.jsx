f6849c437446317250cddedbb4c26dac1dd75cd2
rigole <foplacide@gmail.com>
2021-07-22 01:42:06 +0100

    adding images to Cards

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -3,6 +3,10 @@ import './card.styles.css'
 
 export const Card = (props) => (
     <div className='card-container'>
+        <img
+            alt='monster'
+            src={`https://robohash.org/${props.billionaire.id}1?set=set2&size=180x180`}
+        />
         <h1> {props.billionaire.name} </h1>
     </div>
 )
