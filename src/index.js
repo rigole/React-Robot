687b919c8c623568104a65be2216ce2d0a1dc3d8
rigole <foplacide@gmail.com>
2022-10-10 13:31:12 +0100

    adding redux and setting the constants, actions and reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import {searchRobots} from "./reducer";
+
+const store = createStore(searchRobots)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
